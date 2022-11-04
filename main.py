a1998d015b9bfa6e75da67fd9443d1097343e355
sauluspaulus <43391965+sauluspaulus@users.noreply.github.com>
2022-11-04 10:04:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from webapp import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
 
  
